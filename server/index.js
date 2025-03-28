cc7a118055d88733c56d54fd92cf15eb21ef4fff
SAI SATHWIK <76100488+saisathwik12@users.noreply.github.com>
2025-03-28 11:57:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,16 +15,28 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.options("*", cors());
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://controlhub-mern-project.netlify.app",
+  ];
+  
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000", "http://localhost:3001","https://controlhub-mern-project.netlify.app/"],
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
